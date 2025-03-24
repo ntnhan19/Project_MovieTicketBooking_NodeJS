b2428d698d2b19164e700cffa5c04c793b055d14
ntnhan19 <ngochanpt2018@gmail.com>
2025-03-24 23:13:23 +0700

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+//Middlewares
+app.use(express.json());
+app.use(cors());
+
+//Routes
+
+
+//Error handling middleware
+
+
+//Server running
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
