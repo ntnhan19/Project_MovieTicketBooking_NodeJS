7400a2c772a50b3cd7de522e8089b28f4d411292
ntnhan19 <ngochanpt2018@gmail.com>
2025-06-07 18:44:02 +0700

    [Fix]

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: import.meta.env.VITE_BASE_PATH || "/Project_MovieTicketBooking_NodeJS",
   server: {
-    port: 3002, // Cấu hình frontend chạy ở port 3002
+    port: 3002, 
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // Proxy API về backend
+        target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
       },
